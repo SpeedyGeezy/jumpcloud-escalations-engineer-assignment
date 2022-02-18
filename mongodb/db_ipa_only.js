d678757b2dd380073c206b5156241a49bdb107f1
SpeedyGeezy <31937040+SpeedyGeezy@users.noreply.github.com>
2022-02-18 14:18:51 -0500

    JumpCloud Part 1 Assignment

---
+++ mongodb/db_ipa_only.js
@@ -0,0 +1,31 @@
+// Search for only IPAs in Patrons list
+
+
+// Setting mongodb as client
+var MongoClient = require('mongodb').MongoClient;
+// Setting local connection to mongodb
+var url = "mongodb://127.0.0.1:27017/";
+// Setting mongodb as reference for the subsequent beer type
+var ObjectID = require('mongodb').ObjectID
+var obj = ObjectID('6070941a92f3b0eaf535a86c')
+
+
+// Connect function to MongoDB client - which then passes url in as a parameter - then runs subsequent function
+// dbo set as variable to refer to Brewery Database
+// Query set as variable for either ObjectIDs of the predefined beer types
+// Filter set as variable to include email and remove _id
+// Patrons collection used as reference to query variables
+// Return Results
+// Close
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("brewery");
+  var query = {"favorite_beer":obj};
+  var filter = {email: 1, _id: 0};
+  dbo.collection("patrons").find(query).project(filter).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
